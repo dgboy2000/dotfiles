c98dc7a99edfd6d41427c999267f2753ab3a1b21
Danny Goodman <goodman.danny@gmail.com>
2013-07-02 14:09:19 -0700

    fix install script, update git config

--- install_all.sh
+++ install_all.sh
@@ -1,19 +1,20 @@
 cp .bash_aliases .bash_common .vimrc ~
 
 # Only install gitconfig if one does not exist
-if [ -f ~/.gitconfig ]; then
-  cp .gitconfig ~
-else
+if [ -f ~/.gitconfig ]
+then
   echo "~/.gitconfig already exists; skipping git setup"
+else
+  cp .gitconfig ~/.gitconfig
 fi
 
 # Install .bash_aliases and .bash_common
-cat ~/bash_profile | grep .bash_aliases || echo "
+(cat ~/.bash_profile | grep .bash_aliases || echo "
 if [ -f ~/.bash_aliases ]; then
 . ~/.bash_aliases
-fi" >> ~/.bash_profile
+fi" >> ~/.bash_profile) >> /dev/null
 
-cat ~/bash_profile | grep .bash_common || echo "
+(cat ~/.bash_profile | grep .bash_common || echo "
 if [ -f ~/.bash_common ]; then
 . ~/.bash_common
-fi" >> ~/.bash_profile
+fi" >> ~/.bash_profile) >> /dev/null
