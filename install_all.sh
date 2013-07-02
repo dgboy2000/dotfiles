b54c6868d6d55ec34fc200e65abf673d055b6e6d
Danny Goodman <goodman.danny@gmail.com>
2013-07-02 14:04:20 -0700

    install script checks for git existence first, and properly installs bash common and alias

--- install_all.sh
+++ install_all.sh
@@ -1,1 +1,19 @@
-cp .bash_aliases .bash_common .gitconfig .vimrc ~
+cp .bash_aliases .bash_common .vimrc ~
+
+# Only install gitconfig if one does not exist
+if [ -f ~/.gitconfig ]; then
+  cp .gitconfig ~
+else
+  echo "~/.gitconfig already exists; skipping git setup"
+fi
+
+# Install .bash_aliases and .bash_common
+cat ~/bash_profile | grep .bash_aliases || echo "
+if [ -f ~/.bash_aliases ]; then
+. ~/.bash_aliases
+fi" >> ~/.bash_profile
+
+cat ~/bash_profile | grep .bash_common || echo "
+if [ -f ~/.bash_common ]; then
+. ~/.bash_common
+fi" >> ~/.bash_profile
